46ebffe9ed9cb17ec391dfc376a128671fe2bc93
igorNeves007 <igorneves0404@gmail.com>
2022-10-26 20:54:36 -0300

    Final Commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native'
+
+import { Image, FlatList } from 'react-native'
+import { GameCard, GameCardProps } from '../../components/GameCard'
+import { Heading } from '../../components/Heading'
+import { styles } from './styles'
+import { Background } from '../../components/Background'
+
+const logoImg = require('../../assets/logo-nlw-esports.png')
+
+export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  const navigation = useNavigation()
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl })
+  }
+
+  useEffect(() => {
+    fetch('http://192.168.0.107:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, [])
+
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logoImg} style={styles.logo} />
+
+        <Heading
+          title="Encontre seu duo!"
+          subtitle="Selecione o game que deseja jogar..."
+        />
+
+        <FlatList
+          contentContainerStyle={styles.contentList}
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard data={item} onPress={() => handleOpenGame(item)} /> //Utilizar arrow function para funções que recebem parâmetros
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      </SafeAreaView>
+    </Background>
+  )
+}
